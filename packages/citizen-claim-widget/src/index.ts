32492485c6062e066332c53e5dd0746c306bb707
LewisB <lewis@ikigaistudios.eu>
2026-04-28 16:16:20 +0700

    Add migration template for citizen-sdk

---
+++ packages/citizen-claim-widget/src/index.ts
@@ -0,0 +1,16 @@
+export { citizenClaimIntegration } from './integration'
+export type { CitizenClaimIntegration } from './integration'
+export type {
+  CitizenClaimWidgetAdapterActions,
+  CitizenClaimWidgetAdapterResult,
+  CitizenClaimWidgetAdapterState,
+  CitizenClaimWidgetClientBundle,
+  CitizenClaimWidgetClientFactory,
+  CitizenClaimWidgetClientFactoryInput,
+  CitizenClaimWidgetEnvironment,
+  CitizenClaimWidgetErrorDetail,
+  CitizenClaimWidgetPrimaryAction,
+  CitizenClaimWidgetProps,
+  CitizenClaimWidgetStatus,
+  CitizenClaimWidgetSuccessDetail,
+} from './adapterContract'
