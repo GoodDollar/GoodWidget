7d2dfda868e1147ece066835630f4479273b0538
blueogin <grilucosy@gmail.com>
2026-07-07 08:04:58 -0400

    refactor: remove unused constants and allowance checks in celoPayment for cleaner code

--- packages/ai-credits-widget/src/celoPayment.ts
+++ packages/ai-credits-widget/src/celoPayment.ts
@@ -9,18 +9,11 @@ import {
 
 export const G_TOKEN_CELO_ADDRESS: Address = '0x62B8B11039FcfE5aB0C56E502b1C372A3d2a9c7A'
 
-export const SUPERFLUID_HOST_ADDRESS: Address = '0xA4Ff07cF81C02CFD356184879D953970cA957585'
-
-export const CFA_V1_CELO_ADDRESS: Address = '0x511CBa3De92dB7891967e21Dbd7C4571531ab84B'
-
 export const CFA_V1_FORWARDER_ADDRESS: Address = '0xcfA132E353cB4E398080B9700609bb008eceB125'
 
 const SECONDS_PER_MONTH = 30n * 24n * 3600n
-const MAX_UINT256 = 2n ** 256n - 1n
 
 const G_TOKEN_ABI = parseAbi([
-  'function increaseAllowance(address spender, uint256 addedValue) returns (bool)',
-  'function allowance(address owner, address spender) view returns (uint256)',
   'function transferAndCall(address to, uint256 value, bytes data) returns (bool)',
 ])
 
@@ -28,7 +21,6 @@ const CFA_FORWARDER_ABI = parseAbi([
   'function createFlow(address token, address sender, address receiver, int96 flowrate, bytes userData) returns (bool)',
   'function updateFlow(address token, address sender, address receiver, int96 flowrate, bytes userData) returns (bool)',
   'function getFlowInfo(address token, address sender, address receiver) view returns (uint256 lastUpdated, int96 flowrate, uint256 deposit, uint256 owedDeposit)',
-  'function getBufferAmountByFlowrate(address token, int96 flowrate) view returns (uint256 bufferAmount)',
 ])
 
 const CELO_CHAIN = {
@@ -85,32 +77,6 @@ async function submitStreamSetup(
   })) as readonly [bigint, bigint, bigint, bigint]
   const existingFlowRate = flowInfo[1]
   const flowFunction = existingFlowRate > 0n ? 'updateFlow' : 'createFlow'
-
-  const requiredAllowance = (await publicClient.readContract({
-    address: CFA_V1_FORWARDER_ADDRESS,
-    abi: CFA_FORWARDER_ABI,
-    functionName: 'getBufferAmountByFlowrate',
-    args: [G_TOKEN_CELO_ADDRESS, flowRatePerSecond],
-  })) as bigint
-
-  const existingCfaAllowance = (await publicClient.readContract({
-    address: G_TOKEN_CELO_ADDRESS,
-    abi: G_TOKEN_ABI,
-    functionName: 'allowance',
-    args: [payer, CFA_V1_CELO_ADDRESS],
-  })) as bigint
-
-  if (existingCfaAllowance < requiredAllowance) {
-    const allowanceTx = await walletClient.writeContract({
-      account: payer,
-      chain: CELO_CHAIN,
-      address: G_TOKEN_CELO_ADDRESS,
-      abi: G_TOKEN_ABI,
-      functionName: 'increaseAllowance',
-      args: [CFA_V1_CELO_ADDRESS, MAX_UINT256 - existingCfaAllowance],
-    })
-    txHashes.push(allowanceTx)
-  }
 
   const flowTx = await walletClient.writeContract({
     account: payer,
